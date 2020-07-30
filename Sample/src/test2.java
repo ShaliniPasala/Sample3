21dd9f544d8d1c988527254bea59e61e77197184
Shalini <shalinipasala@gmail.com>
2020-07-30 23:22:58 +0530

    jnj

--- Sample/src/test2.java
+++ Sample/src/test2.java
@@ -3,11 +3,7 @@ public class test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		System.out.println("Him gg");
-=======
-		System.out.println("Him bj");
->>>>>>> branch 'Shalini' of https://github.com/ShaliniPasala/Sample3.git
 
 	}
 
